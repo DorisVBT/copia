1d854eccffb9335041c83ff9af66cbc29d071259
Abel <belo.quintana@gmail.com>
2020-09-05 23:36:27 -0300

    Upload class6

---
+++ Class6-The-Internet/src/test/java/base/BaseTest.java
@@ -0,0 +1,26 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pages.HomePage;
+
+public class BaseTest {
+    protected WebDriver webDriver;
+    protected HomePage homePage;
+
+    @BeforeMethod
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+        webDriver = new ChromeDriver();
+        webDriver.get("https://the-internet.herokuapp.com/");
+        homePage = new HomePage(webDriver);
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        if(webDriver != null)
+            webDriver.quit();
+    }
+}
